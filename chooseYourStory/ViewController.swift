959e154abcb45740e8ecfaaa4461a12a7745101f
phoebeCauld <vikaperova291013@gmail.com>
2021-08-27 17:46:51 +0300

    initial commit

---
+++ chooseYourStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chooseYourStory
+//
+//  Created by F1xTeoNtTsS on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
